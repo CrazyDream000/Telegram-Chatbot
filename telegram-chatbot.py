c91c91ce722480785e2d112454b0660b4f0d9298
CrazyDream000 <danakimoto319@gmail.com>
2024-08-02 17:11:47 -0400

    updated

--- telegram-chatbot.py
+++ telegram-chatbot.py
@@ -7,3 +7,17 @@ telegram_key = "TELEGRAM_API_KEY"
 
 bot = telebot.TeleBot(telegram_key)
 
+def Generate_Respose(prompt):
+    try:
+        completions = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=prompt,
+            max_tokens=150
+        )
+    except:
+        return "Openai server encountered a real time problem.\nPlease try again in some moments"
+    message = completions.choices[0].text
+    message = message.lstrip()
+    return message
+
+
